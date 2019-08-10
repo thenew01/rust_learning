9b85f34e14ccc333e26f7b0b739b36c165d643aa
thenew01 <9091131@qq.com>
2019-08-10 22:54:42 +0800

    添加宏的编写例子

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,8 @@
 */
 
 use std::env::*;
+use std::ptr;
+
 /*use std::path;
 use std::fs;
 use std::io::prelude::*;
@@ -16,16 +18,27 @@ use std::io::BufReader;
 */
 use std::collections::HashMap;
 
-fn process_or_insert(map: &mut HashMap<String, String>, key : String)
+//宏的使用
+macro_rules! hashmap {
+    ( $($key:expr => $val : expr), * ) => {
+        {
+            let mut map = ::std::collections::HashMap::new();
+            //$(map.insert($key, $val);)*
+            $(map.insert($key, $val));*;
+            map
+        }        
+    }
+}
+
+fn process_or_insert(map: &mut HashMap<String, String>, key : String, val: String)
 {
     match map.get_mut(&key)
     {
         Some(value) => println!("{}", value ),
         None => {
-            map.insert(key, String::new());
+            map.insert(key, val);
         }
     }
-
 }
 
 fn main() {
@@ -37,17 +50,25 @@ fn main() {
         //return;
     }
 
+    let counts = hashmap!['a' => 0, 'b' => 1, 'c' => 3];
+    println!("{:?}", counts);
+
 
     let mut map = HashMap::<String, String>::new();
-    let key = String::from("abc");
-    process_or_insert(&mut map, key);
+    let mut key = &String::from("key");
+    let mut val = &String::from("val");
+    process_or_insert(&mut map, key.to_string(), val.to_string());
 
-    for i in  map{
+    for i in map{
         println!("{} {}", i.0, i.1 );
     }
 
-/*
-    //let mut path = env::home_dir().unwrap();
+    println!("{} {}", key, val);    
+    unsafe { ptr::swap( &mut key, &mut val ); }
+    println!("{} {} ", key, val);
+
+    /*
+    let mut path = env::home_dir().unwrap();
     let in_dir = args[1];
     let idx = in_dir.find(".");
     let (in_dir, suffix) = in_dir.split_at(idx);
@@ -55,8 +76,7 @@ fn main() {
 
     for entry in fs::read_dir(in_dir)? {
         let dir = entry?;
-        println!("{:?}", dir.path());
-
+        println!("{}", dir);
     }
 */
 }
